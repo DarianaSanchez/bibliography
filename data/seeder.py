bd644cd3de2968dab5ba93044e0fa5db8c65c455
DarianaSanchez <darianamsanchez@gmail.com>
2023-04-17 13:04:15 -0400

    [REF] refactoring seeder

--- data/seeder.py
+++ data/seeder.py
@@ -1,20 +1,5 @@
 from flask_seeder import Faker, generator
-from .handler import DbHandler
-import functools
-
-
-class Book():
-  def __init__(self, title, author, pages_qty, isbn=None):
-    self.title = title
-    self.author = author
-    self.pages_qty = pages_qty
-    self.isbn = isbn
-
-  def __str__(self):
-    return f'{self.title} by author {self.author}'
-  
-  def get_vals(self):
-    return (self.title, self.author, self.pages_qty, self.isbn)
+from .models import Book, BookPage
 
 
 class DbSeeder():
@@ -33,16 +18,7 @@ class DbSeeder():
 
       # TODO: create as many books as config indicates
       book_batch = 5
-      handler = DbHandler()
-      new_book_cmd = 'INSERT INTO books(title,author,pages_qty,isbn) VALUES(?,?,?,?)'
 
       for book in faker.create(book_batch):
-        # print(f'New book: {book}')
-        new_book_id = handler.insert_record(new_book_cmd, book.get_vals())
-        
-        book_pages_cmd = f'''INSERT INTO pages(book_id,page_number,content)
-                                VALUES{",".join(["(?,?,?)"] * book.pages_qty)}'''
-        book_pages_vals = [(new_book_id, x, f'page {x} content') 
-                            for x in range(1, book.pages_qty + 1)]
-        all_book_pages = functools.reduce(lambda x, y: x + y, book_pages_vals)
-        handler.insert_record(book_pages_cmd, all_book_pages)
+        new_book_id = book.save_book()
+        BookPage.seed_book_pages(new_book_id, book.pages_qty)
