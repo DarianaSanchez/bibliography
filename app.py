a536b082c51ad3c2dd59d2cfd20425b4f3cc78f0
DarianaSanchez <darianamsanchez@gmail.com>
2023-04-17 14:59:07 -0400

    [IMP] api error handling

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from data.models import Book, BookPage
 # from data.handler import DbHandler
 # from data.seeder import DbSeeder
 
-
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
+def response_error(message):
+    return jsonify({'error': message}), 500
 
 @app.route('/')
 def home():
@@ -15,30 +16,39 @@ def home():
 
 @app.route('/books', methods=['GET'])
 def get_all_books():
-    books = Book.get_books()
-    return jsonify(books) 
-
+    try:
+        books = Book.get_books()
+        return jsonify(books)
+    except Exception as ex:
+        return response_error(str(ex))
+ 
 @app.route('/book/<int:book_id>', methods=['GET'])
 def get_book(book_id):
-    book = Book.get_book_by_id(book_id) 
-    return jsonify(book) 
+    try:
+        book = Book.get_book_by_id(book_id) 
+        return jsonify(book) 
+    except Exception as ex:
+        return response_error(str(ex))
 
 @app.route(
         '/book/<int:book_id>/page/<int:page_number>/<string:page_format>',
         methods=['GET']
     )
 def get_book_page(book_id, page_number, page_format):
-    book_page_content = BookPage.get_page_content(book_id, page_number, page_format)
-    response_format = 'text/plain'
+    try:
+        book_page_content = BookPage.get_page_content(book_id, page_number, page_format)
+        response_format = 'text/plain'
 
-    if page_format == 'html':
-        response_format = 'text/html'
+        if page_format == 'html':
+            response_format = 'text/html'
 
-    return Response(
-        response=book_page_content,
-        status=200,
-        mimetype=response_format
-    )
+        return Response(
+            response=book_page_content,
+            status=200,
+            mimetype=response_format
+        )
+    except Exception as ex:
+        return response_error(str(ex))
 
 
 if __name__ == '__main__':
